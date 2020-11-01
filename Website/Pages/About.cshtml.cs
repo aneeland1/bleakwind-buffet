98469599e61361070576015765bdd49fe540919a
Amy <Amy@LAPTOP-KBLAUSMP>
2020-11-01 17:52:35 -0600

    Complete Website Milestone 1

---
+++ Website/Pages/About.cshtml.cs
@@ -0,0 +1,21 @@
+/*
+ * *Author: Amy Neeland
+ *Class name: AboutModel.cshtml.cs
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Website.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Website/Pages/Index.cshtml.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * *Author: Amy Neeland
+ *Class name: IndexModel.cshtml.cs
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BleakwindBuffet.Data.Entrees;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using BleakwindBuffet.Data;
+using BleakwindBuffet.Data.Enums;
+
+namespace Website.Pages
+{
+    public class IndexModel : PageModel
+    {
+        /// <summary>
+        /// gets the list of entrees available
+        /// </summary>
+        public IEnumerable<IOrderItem> Entrees {get { return Menu.Entrees(); } }
+
+        /// <summary>
+        /// gets the list of sides available
+        /// </summary>
+        public IEnumerable<IOrderItem> Sides { get { return Menu.Sides(); } }
+
+        /// <summary>
+        /// gets the list of drinks available
+        /// </summary>
+        public IEnumerable<IOrderItem> Drinks { get { return Menu.Drinks(); } }
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
